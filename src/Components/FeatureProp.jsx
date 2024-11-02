434d8c2c682cacc9d5151d46ec51147ae39b8d55
Esty-source <estyeyere@gmail.com>
2024-11-02 20:23:54 +0100

    initial commit

---
+++ src/Components/FeatureProp.jsx
@@ -0,0 +1,38 @@
+import react from 'react';
+const properties = [
+    {
+        id: 1,
+        title: 'Tranquil Haven in the Woods',
+        price: '$5 970',
+        location: '103 Wright CourtBurien, WA 98168',
+        beds: '4',
+        bath: '3',
+        tag: 'Popular',
+        tagcolor: 'bg-red-500',
+        imageurl: ''
+    },
+
+    {
+        id: 2,
+        title: 'Serene Retreat by The Lake',
+        price: '$1 970',
+        location: '1964 Jehovah Drive, VA 22408',
+        beds: '3',
+        bath: '2',
+        tag: 'New Listing',
+        tagcolor: 'bg-blue-500',
+        imageurl: ''
+    },
+
+    {
+        id: 3,
+        title: 'Charming Cottage in The Meadow',
+        price: '$3 450',
+        location: '1508 Centennial Farm RoadHarlan, 51537',
+        beds: '4',
+        bath: '4',
+        tag: 'Discounted Price',
+        tagcolor: 'bg-green-500',
+        imageurl: ''
+    },
+];
